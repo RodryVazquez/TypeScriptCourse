19d191dbc7d4bc5ef8571e9d2cd5c4dfd36e14cd
rodrigo@rodrigovazquez.com.mx <rodrigo@rodrigovazquez.com.mx>
2017-02-20 01:14:01 -0700

    Se agrego contenido a ts file

--- Practices_TypeScript/Scripts/Helpers/HelpersTs.js
+++ Practices_TypeScript/Scripts/Helpers/HelpersTs.js
@@ -14,4 +14,49 @@ var myself = {
 };
 myself.bankAccount.deposit(1);
 console.log(myself);
+//Arrow Functions
+var addNUmbers = function (number1, number2) { return number1 + number2; };
+console.log(addNUmbers(10, 3));
+var multiplyNumbers = function (number1, number2) { return number1 * number2; };
+console.log(multiplyNumbers(10, 3));
+//Arrow Function sin argumentos
+var greet = function () {
+    console.log("Hello");
+};
+greet();
+var greetFriend = function (friend) { return console.log(friend); };
+console.log(greetFriend("Rodry"));
+//Parametros por default
+var countDown = function (start, end) {
+    if (start === void 0) { start = 20; }
+    if (end === void 0) { end = start - 1; }
+    console.log(start);
+    console.log(end);
+    while (start > 0) {
+        start--;
+    }
+    console.log("Done", start);
+};
+countDown(10);
+//Rest & Spread
+var numbers = [1, 10, 21, 31];
+console.log(Math.max(33, 99, 10));
+console.log(Math.max.apply(Math, numbers));
+//Rest
+function makeArray(name) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray("Rodry", 1, 2, 3));
+//Destructuring array
+var ds = ["Rodry", "Vazquez"];
+var d1 = ds[0], d2 = ds[1];
+console.log(d1, d2);
+//Destructuring object
+var userData = { userName: "Rodry", age: 27 };
+var userName = userData.userName, age = userData.age;
+console.log(userName, age);
 //# sourceMappingURL=HelpersTs.js.map
--- Practices_TypeScript/Scripts/Helpers/HelpersTs.ts
+++ Practices_TypeScript/Scripts/Helpers/HelpersTs.ts
@@ -5,14 +5,14 @@
 //Type del objeto bankAccount
 type bankAccountType = { money: number, deposit: (value: number) => void };
 
-let bankAccount : bankAccountType  = {
+let bankAccount: bankAccountType = {
     money: 2000,
-    deposit(value :number):void {
+    deposit(value: number): void {
         this.money += value;
     }
 };
 
-let myself: { name: string, bankAccount: bankAccountType, hobbies:string[]} = {
+let myself: { name: string, bankAccount: bankAccountType, hobbies: string[] } = {
     name: "Max",
     bankAccount: bankAccount,
     hobbies: ["Sports", "Cooking"]
@@ -20,3 +20,53 @@ let myself: { name: string, bankAccount: bankAccountType, hobbies:string[]} = {
 
 myself.bankAccount.deposit(1);
 console.log(myself);
+
+//Arrow Functions
+const addNUmbers = (number1: number, number2: number): number => number1 + number2;
+console.log(addNUmbers(10, 3));
+
+const multiplyNumbers = (number1: number, number2: number) => number1 * number2;
+console.log(multiplyNumbers(10, 3));
+
+//Arrow Function sin argumentos
+const greet = () => {
+    console.log("Hello");
+};
+greet();
+
+const greetFriend = friend => console.log(friend);
+console.log(greetFriend("Rodry"));
+
+//Parametros por default
+const countDown = (start: number = 20, end:number = start -1): void => {
+    console.log(start);
+    console.log(end);
+    while (start > 0) {
+        start--;
+    }
+    console.log("Done", start);
+};
+countDown(10);
+
+//Rest & Spread
+const numbers = [1, 10, 21, 31];
+console.log(Math.max(33, 99, 10));
+console.log(Math.max(...numbers));
+
+//Rest
+function makeArray(name: string, ...args: number[]) {
+    return args;
+}
+console.log(makeArray("Rodry", 1, 2, 3));
+
+//Destructuring array
+const ds = ["Rodry", "Vazquez"];
+const [d1, d2] = ds;
+console.log(d1, d2);
+
+//Destructuring object
+const userData = { userName: "Rodry", age: 27 };
+const { userName, age } = userData;
+console.log(userName, age);
+
+
